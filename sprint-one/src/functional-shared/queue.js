9da0dacd6a723efa9d5fb1cea5f958c3b68ae0c3
Michael <mpinter09@gmail.com>
2014-09-11 09:16:23 -0700

    finished section 2

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,41 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.count = 0;
+
+  extend(someInstance, queueMethods); // merges stackMethods into someInstance
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
 
+	this[this.count] = value;
+	this.count++;
+	return this.count;
+};
 
+queueMethods.dequeue = function () {
+
+		var temp = this[0];
+		var i = this.count;
+		delete this[0];
+		this.count--;
+		while (i > 1) {
+			this[i - 2] = this[i - 1];
+			i--;
+		}
+		return temp;
+};
+
+queueMethods.size = function () {
+	if (this.count < 0) {
+		this.count = 0;
+	}
+
+	return this.count;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,39 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+
+  extend(someInstance, stackMethods); // merges stackMethods into someInstance
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+stackMethods.pop = function () {
+	var temp = this[this.count - 1];
+	delete this[this.count - 1];
+	this.count--;
+	return temp;
+};
+
+stackMethods.push = function (value) {
+	console.log(this.count, value);
+	this[this.count] = value;
+	this.count++;
+	return value;
+};
+
+stackMethods.size = function () {
+	if (this.count < 0) {
+		this.count = 0;
+	}	
+	return this.count;
+};
