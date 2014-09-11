27be18a8cf699658858ec85572e9c403cd162739
Michael <mpinter09@gmail.com>
2014-09-10 22:43:17 -0700

    finished functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,36 @@
 var makeQueue = function(){
-  var someInstance = {};
-
+ var someInstance = {};
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
-
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    someInstance[value] = value;
+    size++;
+    return value;
   };
 
   someInstance.dequeue = function(){
+    var evalSize = size;
+    var result = storage[0];
+    delete storage[0];
+    delete someInstance[result];
+    size--;
+    while (evalSize > 1){
+      storage[evalSize - 2] = storage[evalSize - 1];
+      evalSize--;
+    }
+    delete storage[size];
+    return result;
   };
 
   someInstance.size = function(){
+  	if(size < 0) {
+  		size = 0;
+  	}
+    return size;
   };
-
+  
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+  	storage[size] = value;
+  	someInstance[value] = value;
+  	size++;
+  	return size;
   };
 
   someInstance.pop = function(){
+  	var temp = storage[--size];
+  	delete storage[size];
+  	delete someInstance[temp];
+  	return temp;
   };
 
   someInstance.size = function(){
+  if (size < 0) {
+  	size = 0;
+  }
+  return size;
   };
 
   return someInstance;
